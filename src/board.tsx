55dcbe8e28b5aff72823d5cef64974bf5cca5fd6
Aiden Blinn <apblinn@gmail.com>
2024-06-03 18:16:05 -0400

    attach onclick property to grid items to allow robot placement

--- src/board.tsx
+++ src/board.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { GRIDSIZE } from "./constants";
+import * as CONSTANTS from "./constants.ts";
+import * as TYPES from "../types.ts";
 
-export default function Board(): React.ReactElement {
+export default function Board({
+  placeRobot,
+}: {
+  placeRobot: (position: TYPES.RobotPosition) => void;
+}): React.ReactElement {
   const tiles = [];
-  for (let i = 0; i < GRIDSIZE; i++) {
-    for (let j = 0; j < GRIDSIZE; j++) {
-      const tileNumber = i + j * GRIDSIZE;
+  for (let row = CONSTANTS.GridSize - 1; row >= 0; row -= 1) {
+    for (let col = 0; col < CONSTANTS.GridSize; col += 1) {
+      const tileNumber = row + col * CONSTANTS.GridSize;
       tiles.push(
         <div
           key={`robot-board-box-${tileNumber}`}
           className={tileNumber % 2 === 0 ? "bg-green-600" : "bg-green-400"}
-        />
+          onClick={() => placeRobot({ x: row, y: col })}
+        >
+          {row} {col}
+        </div>
       );
     }
   }
