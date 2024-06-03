5356c07e8e7af688bcf786e3deef548b20a237ed
Aiden Blinn <apblinn@gmail.com>
2024-06-03 17:29:51 -0400

    initial repo setup with tailwind/react/typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="flex h-screen justify-center align-center">
+      <p>Initial setup</p>
+    </div>
+  );
+}
+
+export default App;
