5228ef012cbcfa3420a870f1ccf1e4a882f52e0e
Aiden Blinn <apblinn@gmail.com>
2024-06-03 18:51:57 -0400

    rename component and replace robot image to better match prompt

--- src/tabletop.tsx
+++ src/tabletop.tsx
@@ -2,23 +2,27 @@ import React from "react";
 import * as CONSTANTS from "./constants.ts";
 import * as TYPES from "../types.ts";
 
-export default function Board({
+export default function Tabletop({
   placeRobot,
 }: {
   placeRobot: (position: TYPES.RobotPosition) => void;
 }): React.ReactElement {
   const tiles = [];
+  // Iterate over rows and columns of tabletop
   for (let row = CONSTANTS.GridSize - 1; row >= 0; row -= 1) {
     for (let col = 0; col < CONSTANTS.GridSize; col += 1) {
       const tileNumber = row + col * CONSTANTS.GridSize;
       tiles.push(
+        // Add individual grid tile
+        // Move robot to this tile when tile clicked
         <div
-          key={`robot-board-box-${tileNumber}`}
-          className={tileNumber % 2 === 0 ? "bg-green-600" : "bg-green-400"}
+          key={`robot-tabletop-box-${tileNumber}`}
+          className={
+            "hover:bg-blue-400" +
+            (tileNumber % 2 === 0 ? " bg-green-600" : " bg-green-400")
+          }
           onClick={() => placeRobot({ x: row, y: col })}
-        >
-          {row} {col}
-        </div>
+        />
       );
     }
   }
